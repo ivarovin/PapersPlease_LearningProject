ff7b69733fb62563e8db16fcaf29259a67c701c4
RGV <ricardo.guzman.velasco@outlook.com>
2022-11-27 21:31:59 +0100

    chore: recreate officer desk in scene

--- Assets/PapersPlease/Runtime/View/CanvasNewspaper.cs
+++ Assets/PapersPlease/Runtime/View/CanvasNewspaper.cs
@@ -17,14 +17,15 @@ namespace PapersPlease.Runtime.View
             gameObject.SetActive(true);
             date.text = $"{dateTime:dd/MM/yyyy}";
 
+            GetComponent<CanvasGroup>().DOFade(1, .2f);
             transform.DOLocalRotate(Vector3.back * 360 * 9, .9f, RotateMode.LocalAxisAdd).SetEase(OutQuad);
             await transform.DOScale(0, .9f).From().SetEase(InQuad).AsyncWaitForCompletion();
         }
 
         public async Task Close()
         {
+            await GetComponent<CanvasGroup>().DOFade(0, .45f).AsyncWaitForCompletion();
             gameObject.SetActive(false);
-            await Task.Delay(799);
         }
     }
 }
