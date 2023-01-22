2b94bd6befe38983dd98d87205bd6b5701c4889b
RGV <ricardo.guzman.velasco@outlook.com>
2023-01-22 10:57:21 +0100

    refactor: extract food and heat into EconomicBalance subclass

--- Assets/PapersPlease/Runtime/Model/EconomicBalance.cs
+++ Assets/PapersPlease/Runtime/Model/EconomicBalance.cs
@@ -6,12 +6,8 @@
         public float Savings { get; init; }
         public float Rent { get; init; }
         public float Penalties { get; init; }
-        public float Heat { get; init; }
-        public float Food { get; init; }
-        public bool ExpensesInHeat { get; init; }
-        public bool ExpensesInFood { get; init; }
-        
-        public float TotalExpenses => Rent + Penalties + (ExpensesInHeat ? Heat : 0) + (ExpensesInFood ? Food : 0);
+
+        public virtual float TotalExpenses => Rent + Penalties;
         public float TotalIncome => Salary + Savings;
         public float Balance => TotalIncome - TotalExpenses;
         public bool IsInDebt => Balance < 0;
